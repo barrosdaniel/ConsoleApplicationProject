76b572520fd32b2eef652c1940c3fa6d16ed9dc4
Daniel Barros <daniel.barros@cqumail.com>
2022-08-12 23:02:39 +0800

    Create HeavyVehicle class methods and unit test.

--- VehicleRegistration/HeavyVehicle.java
+++ VehicleRegistration/HeavyVehicle.java
@@ -2,4 +2,33 @@ package VehicleRegistration;
 
 public class HeavyVehicle extends Vehicle {
     private int loadCapacity;
+
+    public HeavyVehicle() {
+        this("Plate number not provided", "Make not provided", "Model not " +
+                "provided", 1900, false, 0, 0, 0);
+    }
+
+    public HeavyVehicle(String plateNumber, String make, String model, int year,
+                        boolean isPrivate, int ownerId, int ownerABN,
+                        int loadCapacity) {
+        super(plateNumber, make, model, year, isPrivate, ownerId, ownerABN);
+        setLoadCapacity(loadCapacity);
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HeavyVehicle {Plate number: %s, Make: %s, " +
+                        "Model: %s, Year: %d, Private: %b, Owner ID: %d, Owner ABN: " +
+                        "%d, Load Capacity: %d}", getPlateNumber(), getMake(),
+                getModel(), getYear(), isPrivate(), getOwnerId(),
+                getOwnerABN(), getLoadCapacity());
+    }
 }
--- VehicleRegistration/UnitTest.java
+++ VehicleRegistration/UnitTest.java
@@ -9,8 +9,10 @@ public class UnitTest {
     public static void main(String[] args) {
         owners.add(new PrivateOwner("Jamie Harold", "1 Macquarie St", "0411 111 111", 1001, "20/01/2000"));
         owners.add(new CorporateOwner("Jamal Abdul", "1 Ferris Cl", "0422 222 222", 123456789));
-        vehicles.add(new LightVehicle("1XL123", "Fromyota", "SUV-2", 2020,
+        vehicles.add(new LightVehicle("1XS789", "Fromyota", "SUV-2", 2020,
                 true, 98764321, 1512357895, 7));
+        vehicles.add(new HeavyVehicle("1XL123", "Toto", "T-5000", 2020,
+                false, 555999333, 1234567891, 5000));
 
         for (Owner o : owners) {
             System.out.println(o);
