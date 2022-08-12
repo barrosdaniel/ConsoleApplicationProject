28500a927e20d5129e395db74c0474209b2f7112
barrosdaniel <barros.daniel@outlook.com>
2022-08-12 21:26:39 +0800

    Create CorporateOwner class methods and unit test.

--- VehicleRegistration/CorporateOwner.java
+++ VehicleRegistration/CorporateOwner.java
@@ -2,4 +2,26 @@ package VehicleRegistration;
 
 public class CorporateOwner extends Owner {
     private int abn;
+
+    public CorporateOwner() {
+        this("Name not supplied", "Address not supplied", "Phone number not supplied", 0);
+    }
+
+    public CorporateOwner(String name, String address, String phoneNumber, int abn) {
+        super(name, address, phoneNumber);
+        setAbn(abn);
+    }
+
+    public int getAbn() {
+        return abn;
+    }
+
+    public void setAbn(int abn) {
+        this.abn = abn;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("CorporateOwner {Name: %s, Address: %s, Phone number: %s, ABN: %d}", getName(), getAddress(), getPhoneNumber(), getAbn());
+    }
 }
--- VehicleRegistration/UnitTest.java
+++ VehicleRegistration/UnitTest.java
@@ -8,6 +8,7 @@ public class UnitTest {
 
     public static void main(String[] args) {
         owners.add(new PrivateOwner("Jamie Harold", "1 Macquarie St", "0411 111 111", 1001, "20/01/2000"));
+        owners.add(new CorporateOwner("Jamal Abdul", "1 Ferris Cl", "0422 222 222", 123456789));
 
         for (Owner o : owners) {
             System.out.println(o);
